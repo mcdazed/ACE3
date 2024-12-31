428fc3bf5bb0520c735c982aa3ecc5b5a76b4bee
Administrator <PC@mcdazed.com>
2024-12-31 15:59:06 -0600

    [Administrator] [15:59:06.14] [Tue 12/31/2024]

--- addons/medical_engine/script_macros_medical.hpp
+++ addons/medical_engine/script_macros_medical.hpp
@@ -54,11 +54,11 @@
 #define BLOOD_VOLUME_CLASS_3_HEMORRHAGE 4.200 // lost more than 30% blood, Class III Hemorrhage
 #define BLOOD_VOLUME_CLASS_4_HEMORRHAGE 3.600 // lost more than 40% blood, Class IV Hemorrhage
 // Lost more than 50% blood, Unrecoverable
-#define BLOOD_VOLUME_FATAL 3.0
+#define BLOOD_VOLUME_FATAL 3.6
 
 // Minimum blood volume, in liters, for a patient to have the chance to wake up
 #define MINIMUM_BLOOD_FOR_STABLE_VITALS EGVAR(medical,const_stableVitalsBloodThreshold)
-#define MINIMUM_BLOOD_FOR_STABLE_VITALS_DEFAULT BLOOD_VOLUME_CLASS_2_HEMORRHAGE
+#define MINIMUM_BLOOD_FOR_STABLE_VITALS_DEFAULT BLOOD_VOLUME_CLASS_3_HEMORRHAGE
 
 // IV Change per second calculation:
 // 250 ml should take 60 seconds to fill. 250 ml / 60 s ~ 4.1667 ml/s.
